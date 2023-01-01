92a6c5f211c7c0340cc9a48041e22974f59bd2c8
unknown <fredericreact@gmail.com>
2023-01-01 17:06:32 +0800

    Adding Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React, {Suspense} from 'react'
 import {Route, Switch, Redirect} from 'react-router-dom'
-import AllQuotes from './pages/AllQuotes';
-import QuotesDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
 import Layout from './components/layout/Layout';
-import NotFound from './pages/NotFound';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuote = React.lazy(()=> import('./pages/NewQuote'));
+const QuotesDetail = React.lazy(()=> import('./pages/QuoteDetail'));
+const NotFound = React.lazy(()=> import('./pages/NotFound'))
+const AllQuotes=React.lazy(()=> import('./pages/AllQuotes'))
 
 function App() {
   return (
     <Layout>
+    <Suspense
+    fallback={
+      <div className='centered'>
+        <LoadingSpinner/>
+      </div>
+    }>
     <Switch>
 
     <Route path='/' exact>
@@ -31,6 +40,7 @@ function App() {
       </Route>
 
     </Switch>
+    </Suspense>
     </Layout>
   );
 }
